9ee6b74f11051b6bef2e34090cb6316a4b6e1a14
Kyle <kyruken@gmail.com>
2023-08-05 17:02:49 -0700

    finish bullet

--- Assets/MonsterBulletScript.cs
+++ Assets/MonsterBulletScript.cs
@@ -7,42 +7,28 @@ public class MonsterBulletScript : MonoBehaviour
     public PlayerHealthScript playerHealth;
     public float bulletSpeed = 1f;
     public GameObject player;
+    private Rigidbody2D rb;
     private float currentPlayerXPos;
     private float currentPlayerYPos;
     private bool haveSpawned = false;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthScript>();
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * bulletSpeed;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!haveSpawned)
-        {
-            currentPlayerXPos = player.transform.position.x;
-            currentPlayerYPos = player.transform.position.y;
-            haveSpawned = true;
-        }
         
-        if (transform.position.x < currentPlayerXPos) {
-            transform.position = transform.position + (Vector3.right * bulletSpeed) * Time.deltaTime;
-        } else {
-            transform.position = transform.position + (Vector3.left * bulletSpeed) * Time.deltaTime;
-        }
-
-        if (transform.position.y < currentPlayerYPos) {
-            transform.position = transform.position + (Vector3.up * bulletSpeed) * Time.deltaTime;
-        } else {
-            transform.position = transform.position + (Vector3.down * bulletSpeed) * Time.deltaTime;
-        }
-
-        if (transform.position.x == currentPlayerXPos && transform.position.y == currentPlayerYPos)
-        {
-            Destroy(gameObject);
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) 
@@ -50,6 +36,10 @@ public class MonsterBulletScript : MonoBehaviour
         if (collision.gameObject.tag == "Player") 
         {
             playerHealth.takeDamage(1);
-        } 
+            Destroy(gameObject);
+        } else if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
     }
 }
